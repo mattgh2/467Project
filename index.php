c2ed90666b785f1b7444f32c3cc088d79cd94d92
mattgh2 <mattwarner744@gmail.com>
2025-04-04 00:01:34 +0000

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    $host = "blitz.cs.niu.edu";
+    $dbname = "csci467";
+    $username = "student";
+    $password = "student";
+
+    try {
+        $dsn = "mysql:host=$host;dbname=$dbname";
+        $pdo = new pdo($dsn,$username,$password);
+    }
+    catch (PDOException $e) {
+        echo "failed to connect to the database. " . $e->getMessage();
+    }
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="./src/output.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Test...</title>
+</head>
+
+<body class="bg-indigo-500">
+    <h1>halo....</h1>
+</body>
+
+</html>
