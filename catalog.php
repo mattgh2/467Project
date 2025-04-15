b8a470468450ee0ba661c5ac5c9c47db1652dc20
mattgh2 <mrpizzaman856@gmail.com>
2025-04-15 04:25:38 +0000

    some stuff

--- catalog.php
+++ catalog.php
@@ -1,6 +1,87 @@
-<?php
-    include "dbconnect.php"
+00<?php // Error stuff
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 ?>
+<?php
+    include "dbconnect.php";
+
+    function  query_parts() : array {
+       global $pdoLegacy;
+       return $pdoLegacy->query("select * from parts")->fetchAll(PDO::FETCH_ASSOC);
+    }
+    $parts = query_parts();
+    $parts_0_url = $parts[0]['pictureURL'];
+    echo "<img src=\"$parts_0_url\"></a>";
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="./src/output.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta http-equiv="refresh" content="1">
+    <title>Lite Up Ur Lyfe Auto Parts</title>
+</head>
+
+<body class="w-screen h-screen m-0 bg-pink-100">
 
 <?php // functions
 ?>
+<?php
+    echo <<<END
+    <div class="w-full bg-white h-[7%] flex shadow-xl mb-[2%]">
+        <div class="w-[10%] h-full flex justify-center items-center">
+             <h1 class="flex justify-center items-center  ml-[1%]"> <a id='home-page' href="./index.php"> Home </a> </h1>
+        </div>
+        <div class="w-full h-full flex justify-end items-center mr-[2%]"> 
+            <ul class="flex h-full items-center justify-center gap-10">
+            <li class=""> <a href="./catalog.php" class="hover:text-green-500 cursor-pointer"> Catalog </a> </li>
+            <li> <a> <i class="fa fa-lock" aria-hidden="true"></i> Warehouse </a> </li>
+            <li> <a> <i class="fa fa-lock" aria-hidden="true"></i> Admin </a> </li>
+            <li class="text-2xl"> <a> <i class="fa fa-shopping-bag" aria-hidden="true"></i> </a> </li>
+        </ul>
+        </div>
+    </div>
+    END;
+?>
+<?php 
+    echo<<<END
+    <div class="w-full h-1/2 flex">
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl flex justify-center">
+    END;
+        echo "<div class=\"w-[90%] h-[40%] bg-red-200 mt-[5%]\">";
+            </div>
+        </div>
+    END;
+
+
+    echo<<<END
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl">
+        </div> 
+
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl">
+        </div>
+
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] mr-[1%] rounded-3xl shadow-2xl">
+        </div>    
+    </div>    
+    <div class="w-full h-1/2 flex mt-[2%]">
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl">
+        </div>
+  
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl">
+        </div> 
+
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] rounded-3xl shadow-2xl">
+        </div>
+
+        <div class="w-[25%] h-full bg-green-300 ml-[1%] mr-[1%] rounded-3xl shadow-2xl">
+        </div>    
+    </div> 
+    END;
+?>
+
+</body> </html>/html>
--- index.php
+++ index.php
@@ -9,35 +9,37 @@
 
 <html>
 <head>
+    <!-- Favicon links --> 
+    <link rel="icon" href="asset/favicon.ico" type="image/x-icon" /> 
+
     <link rel="stylesheet" href="./src/output.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
     <meta http-equiv="refresh" content="1">
     <title>Lite Up Ur Lyfe Auto Parts</title>
+    
 </head>
 
-<body class="w-screen h-screen m-0 bg-gray-800"></body>
+<body class="w-screen min-h-screen m-0 bg-white">
 
 <?php // Functions
 ?>
-<?php
-    echo <<<END
-        <div class="w-full bg-white h-[7%] flex shadow-xl">
-            <div class="w-[10%] h-full flex justify-center items-center">
-                 <h1 class="flex justify-center items-center  ml-[1%]"> <a id='home-page' href="./"> Home </a> </h1>
-            </div>
-            <div class="w-full h-full flex justify-end items-center mr-[2%]"> 
+    <div class="w-full bg-[#55baf2] h-[7%] flex shadow-xl">
+        <div class="w-[5%] h-full flex justify-center items-center">
+            <a id='home-page' href="./" class="text-3xl"> <i class="fa fa-home" aria-hidden="true"></i> </a>
+        </div>
+        <div class="w-full h-full  flex justify-end items-center mr-[2%]"> 
                 <ul class="flex h-full items-center justify-center gap-10">
-                <li class=""> <a href="./catalog.php" class="hover:text-green-500 cursor-pointer"> Catalog </a> </li>
-                <li> <a> <i class="fa fa-lock" aria-hidden="true"></i> Warehouse </a> </li>
-                <li> <a> <i class="fa fa-lock" aria-hidden="true"></i> Admin </a> </li>
-                <li class="text-2xl"> <a> <i class="fa fa-shopping-bag" aria-hidden="true"></i> </a> </li>
+                <li class=""> <a href="./catalog.php" class="hover:text-green-500 cursor-pointer transition-colors duration-100 ease-in-out"> Catalog </a> </li>
+                <li> <a class="hover:text-green-500 cursor-pointer transition-colors duration-100 ease-in-out"> <i class="fa fa-lock" aria-hidden="true"></i> Warehouse </a> </li>
+                <li> <a class="hover:text-green-500 cursor-pointer transition-colors duration-100 ease-in-out"> <i class="fa fa-lock" aria-hidden="true"></i> Admin </a> </li>
+                <li class="text-2xl"> <a class="hover:text-green-500 cursor-pointer transition-colors duration-100 ease-in-out"> <i class="fa fa-shopping-bag" aria-hidden="true"></i> </a> </li>
             </ul>
-            </div>
-        <div>
-        
-    END;
-?>
+        </div>
+    </div>
+    <section class="w-full bg-red-100 h-80">
+
+    </section>
 
 </body> </html> 
